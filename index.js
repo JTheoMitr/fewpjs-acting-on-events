45f3f23602136c997fd9306cf6aaf116129d9b7f
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2021-03-30 01:31:11 -0500

    updated and finished

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function moveDodgerLeft() {
     let leftNumbers = dodger.style.left.replace("px", "");
     let left = parseInt(leftNumbers, 10);
   
-    if (left < 400) {
+    if (left < (document.getElementById("game").clientWidth) - 40) {
       dodger.style.left = `${left + 1}px`;
     }
   } 
